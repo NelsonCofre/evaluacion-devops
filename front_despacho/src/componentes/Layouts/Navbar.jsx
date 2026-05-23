28ca3f2d83c34e6147284c43f1a0ede9ddcab3df
NelsonCofre <nelsoncofrew@gmail.com>
2026-05-22 21:22:36 -0400

    elemento nuevo en sidebar

--- front_despacho/src/componentes/Layouts/Navbar.jsx
+++ front_despacho/src/componentes/Layouts/Navbar.jsx
@@ -3,6 +3,7 @@ function Navbar() {
     <nav className="rounded-xl w-[250px] min-h-[880px] bg-teal-600 text-white sticky top-0 p-4 m-4">
       {/* Logo o título */}
       <h2 className="text-xl font-bold mb-8">Despacho Dashboard</h2>
+      <p className="text-sm text-teal-100 mb-8">Demo CI/CD — Innovatech Chile</p>
 
       {/* Menú de navegación */}
       <ul className="space-y-3">
